6998fcfac208c718679730f8f85419a5f6875f20
Aquib Master <aquibmaster@gmail.com>
2016-10-09 16:48:57 +1300

    Remove unused routes

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { config } from 'config'
 
 export default class Index extends React.Component {
   render () {
+    console.log(this.props);
     return (
       <div>
         <Helmet
@@ -16,49 +17,10 @@ export default class Index extends React.Component {
           ]}
         />
         <h1>
-          Hi people
+          Hello world
         </h1>
-        <p>Welcome to your new Gatsby site</p>
-        <h2>Below are some pages showing different capabilities built-in to Gatsby</h2>
-        <h3>Supported file types</h3>
-        <ul>
-          <li>
-            <Link to={prefixLink('/markdown/')}>Markdown</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/react/')}>JSX (React components)</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/coffee-react/')}>CJSX (Coffeescript React components)</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/html/')}>HTML</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/json/')}>JSON</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/yaml/')}>YAML</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/toml/')}>TOML</Link>
-          </li>
-        </ul>
-        <h3>Supported CSS processors</h3>
-        <ul>
-          <li>
-            <Link to={prefixLink('/postcss/')}>PostCSS</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/css-modules/')}>CSS Modules</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/sass/')}>Sass</Link>
-          </li>
-          <li>
-            <Link to={prefixLink('/less/')}>Less</Link>
-          </li>
-        </ul>
+
+        <Link to={prefixLink('/css-modules/')}>Example page with CSS modules</Link>
       </div>
     )
   }
