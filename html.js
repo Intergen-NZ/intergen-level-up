4b6c691ac7a00dec659ceb7c6b25bf33f8cb8a63
Aquib Master <aquibmaster@gmail.com>
2016-10-09 17:21:37 +1300

    Remove more bloat

--- html.js
+++ html.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Helmet from "react-helmet"
 
 import { prefixLink } from 'gatsby-helpers'
-import { TypographyStyle, GoogleFont } from 'react-typography'
-import typography from './utils/typography'
 
 const BUILD_TIME = new Date().getTime()
 
@@ -32,8 +30,6 @@ module.exports = React.createClass({
           />
           {head.title.toComponent()}
           {head.meta.toComponent()}
-          <TypographyStyle typography={typography} />
-          <GoogleFont typography={typography} />
           {css}
         </head>
         <body>
--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { Container } from 'react-responsive-grid'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Headroom from 'react-headroom'
-
-import { rhythm } from '../utils/typography'
 
 module.exports = React.createClass({
   propTypes () {
@@ -15,42 +12,11 @@ module.exports = React.createClass({
   render () {
     return (
       <div>
-        <Headroom
-          wrapperStyle={{
-            marginBottom: rhythm(1),
-          }}
-          style={{
-            background: 'lightgray',
-          }}
-        >
-          <Container
-            style={{
-              maxWidth: 960,
-              paddingTop: 0,
-              padding: `${rhythm(1)} ${rhythm(3/4)}`,
-            }}
-          >
-            <Link
-              to={prefixLink('/')}
-              style={{
-                color: 'black',
-                textDecoration: 'none',
-              }}
-            >
-              Gatsby!!!
-            </Link>
-          </Container>
+        <Headroom>
+          <Link to={prefixLink('/')}>Gatsby!!!</Link>
         </Headroom>
-        <Container
-          style={{
-            maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(3/4)}`,
-            paddingTop: 0,
-          }}
-        >
-          {this.props.children}
-        </Container>
+        {this.props.children}
       </div>
-    )
+    );
   },
 })
