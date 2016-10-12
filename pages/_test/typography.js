5d7ae6bd16eb2aa7059f87ec48354a1cb41ce3a3
Aquib Master <aquibmaster@gmail.com>
2016-10-12 22:34:29 +1300

    Add test route to surface typography

---
+++ pages/_test/typography.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Typography = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>Hello world</h2>
+            <h3>Hello world</h3>
+            <h4>Hello world</h4>
+            <h5>Hello world</h5>
+            <h6>Hello world</h6>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil vitae velit optio iure reprehenderit possimus, suscipit quibusdam minus cum illum expedita, doloribus, odit aliquid facilis eos maiores ducimus, quo! Quisquam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil vitae velit optio iure reprehenderit possimus, suscipit quibusdam minus cum illum expedita, doloribus, odit aliquid facilis eos maiores ducimus, quo! Quisquam.</p>
+
+            <ul>
+                <li>Hello</li>
+                <li>World</li>
+                <li>Top kek</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Typography;
