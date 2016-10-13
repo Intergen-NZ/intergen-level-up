983cda6d88763ed1cb27fe25fed7bd9b415ca120
Aquib Master <Aquib.Master@intergen.co.nz>
2016-10-13 17:16:00 +1300

    Redesign navbar and static content on the index page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,23 +2,16 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 
+import logo from './logo.png';
 import styles from './navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className={styles.wrap}>
-            <div className={styles.container}>
-                <div className={styles.navLeft}>
-                    <Link to={prefixLink('/')}>Intergen: Level up</Link>
-                </div>
-
-                <div className={styles.navRight}>
-                    <ul>
-                        <li><Link to={prefixLink('/')}>Home</Link></li>
-                        <li><Link to={prefixLink('/')}>About</Link></li>
-                        <li><Link to={prefixLink('/')}>Mailing List</Link></li>
-                    </ul>
-                </div>
+        <nav className={styles.root}>
+            <div className={styles.brand}>
+            	<Link to={prefixLink('/')} className={styles.logo}>
+	            	<img src={logo} width="200" /><span>evolve</span>
+	            </Link>
             </div>
         </nav>
     );
