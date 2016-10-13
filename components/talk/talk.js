2d33719e682e424c9e486dcfd0f805ec44cb7191
Aquib Master <Aquib.Master@intergen.co.nz>
2016-10-13 16:20:50 +1300

    Refactor to pull out a talks component. Misc styling changes

---
+++ components/talk/talk.js
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { prefixLink } from 'gatsby-helpers';
+
+import styles from './talk.module.css';
+
+const Talk = ({ path, data }) => {
+	// TODO(AM): Pull this out & write some tests lol.
+	let summary = data.description;
+	if(summary.length > 250) {
+		const postSummaryDescription = summary.substring(250, summary.length);
+		summary = summary.substring(0, postSummaryDescription.indexOf(' ') + 250) + '...';
+	}
+
+	return (
+		<div className={styles.talk}>
+			<Link to={prefixLink(path)}><h6 className={styles.title}>{data.title}</h6></Link>
+            <div>
+            	<span>{data.speaker}</span>
+            </div>
+			<p className={styles.summary}>{summary}</p>
+		</div>
+	);
+};
+
+Talk.propTypes = {
+	data: PropTypes.object.isRequired,
+	path: PropTypes.string.isRequired
+};
+
+export default Talk;
