fe3912b32e5522cc19032aa87a1dc3dc31a696cf
Aquib Master <aquibmaster@gmail.com>
2017-01-31 20:12:21 +1300

    Add meta data, slide deck and link dump to the talk page

--- wrappers/json.js
+++ wrappers/json.js
@@ -12,6 +12,14 @@ class JsonWrapper extends React.Component {
 	render() {
 		const data = this.props.route.page.data;
 
+        const linkDump = data.links.map(link => {
+            return (
+                <li>
+                    <a href={link.href} target="_blank">{link.title}</a>
+                </li>
+            );
+        });
+
 		return (
 			<div className={styles.root}>
 				<Helmet title={`${config.siteTitle} | ${data.title} - ${data.speaker}`} />
@@ -21,10 +29,28 @@ class JsonWrapper extends React.Component {
                 <div className={styles.talkWrap}>
                     <div className={styles.talk}>
                         <h3 className={styles.title}>{data.title}</h3>
+                        <p className={styles.meta}>
+                            <span className={styles.speaker}>{data.speaker}</span>
+                            <span className={styles.date}>{data.date}</span>
+                        </p>
+
                         <p className={styles.description}>
                             {data.description}
                         </p>
 
+                        <h6>Slide Deck</h6>
+                        <a href={data.slideDeck.link} target="_blank">{data.slideDeck.link}</a>
+
+                        { linkDump.length > 0 &&
+                            <div>
+                                <h6>Link Dump</h6>
+                                <ul className={styles.linkDumpList}>
+                                    {linkDump}
+                                </ul>
+                            </div>
+                        }
+
+                        
                     </div>
                 </div>
 			</div>
